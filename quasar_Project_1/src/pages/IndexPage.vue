91e0542a34f0069c12e3242940aa74cfccaa096c
unknown <yotsakorn.354@gmail.com>
2026-07-10 14:55:32 +0700

    Edited index.vue with alt -1-hello world

--- quasar_Project_1/src/pages/IndexPage.vue
+++ quasar_Project_1/src/pages/IndexPage.vue
@@ -2,7 +2,7 @@
   <q-page class="flex flex-center">
     <div class="column items-center">
       <img
-        alt="Quasar logo"
+        alt="Quasar logo -1-hello world"
         src="~@/assets/quasar-logo-vertical.svg"
         style="width: 200px; height: 200px"
       />
