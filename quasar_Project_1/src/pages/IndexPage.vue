9c68ca649b611825be1e379083b23d033484b600
Yotsakorn <kmegumi354@gmail.com>
2026-07-24 14:44:03 +0700

    Edited Thai language in indexpage.vue

--- quasar_Project_1/src/pages/IndexPage.vue
+++ quasar_Project_1/src/pages/IndexPage.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="q-pa-md">
     <div class="q-gutter-y-md column" style="max-width: 300px">
-      <q-input color="purple-12" v-model="text" label="Label">
+      <q-input color="purple-12" v-model="text" label="Label(ชื่อสีม่วง)">
         <template v-slot:prepend>
           <q-icon name="event" />
         </template>
       </q-input>
 
-      <q-input color="teal" filled v-model="text" label="Label">
+      <q-input color="teal" filled v-model="text" label="Label(นามสกุลสีเขียว)">
         <template v-slot:prepend>
           <q-icon name="event" />
         </template>
